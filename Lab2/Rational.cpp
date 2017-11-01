6e252423b0feeeb1f9a5073a3f98c4a01ceccf2b
coltinelson <coltinelson@gmail.com>
2017-11-01 14:23:52 -0300

    Lab2

---
+++ Lab2/Rational.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+class Rational {
+private:
+    int num;
+    int denom;
+
+    Rational::Rational(int num, int denom) {
+
+    }
+}
