fd484404de62dc5faed7ea9578fc6450765f6999
coltinelson <coltinelson@gmail.com>
2017-11-23 11:24:40 -0400

    Lab 4

--- Lab4/HTMLConverter.cpp
+++ Lab4/HTMLConverter.cpp
@@ -1,78 +1,98 @@
 #include "HTMLConverter.h"
-#include <iostream>
 #include <fstream>
 
 using namespace std;
 
+//check if input file is .cpp
 bool HTMLConverter::checkInputFileName(string name) {
     bool valid = false;
+    //get the length of the file name
     int length = name.length();
+    //get the last 4 characters
     string extension = name.substr(length-4, length-1);
+    //check to see if that substring is .cpp
     if (extension == ".cpp") {
+        //if it is, its valid
         valid = true;
-        cout << "valid" << endl;
     }
-
+    //otherwise, its not
     return valid;
 }
 
+//check if input file is .html
 bool HTMLConverter::checkOutputFileName(string name) {
     bool valid = false;
+    //get the length of the file name
     int length = name.length();
+    //get the last 5 characters
     string extension = name.substr(length-5, length-1);
+    //check to see if that substring is .html
     if (extension == ".html") {
+        //if it is, its valid
         valid = true;
     }
-
+    //otherwise, its not
     return valid;
 }
 
+//read in the file to ensure it actually exists
 bool HTMLConverter::readFile(string name) {
-    bool valid = false;
 
+    //create the ifstream
     ifstream inputFile;
+    //set up the exceptions
     inputFile.exceptions(ifstream::failbit | ifstream::badbit);
 
+    //try to open the file
+    //if it opens, return true otherwise return false;
     try {
         inputFile.open(name);
-        valid = true;
+        return true;
     }
     catch(ifstream::failure&) {
-        valid = false;
+        return false;
     }
-
-    return valid;
 }
 
+//write to the file including converted strings
 string HTMLConverter::writeToFile(string inputName, string outputName) {
 
+    //open the input and output streams
     ifstream inputFile;
     inputFile.exceptions(ifstream::failbit | ifstream::badbit);
 
     ofstream outputFile(outputName);
 
     try {
+        //try to open the file
         inputFile.open(inputName);
 
+        //if the file is open, continue on
         if (inputFile.is_open()) {
             string line;
 
+            //if the output file doesn't fail, add a pre tag to the beginning
             if (!outputFile.fail()) {
                 outputFile << "<PRE>" << endl;
             }
+            //read in each line of the input file
             while (getline(inputFile,line)) {
                 if (!inputFile.fail()) {
+                    //send the line through the converter
                     line = HTMLConverter::convertLine(line);
                     if (!outputFile.fail()) {
+                        //add the line to the output file
                         outputFile << line << endl;
                     }
                 }
             }
+            //if the output file doesn't fail, add a closing pre tag to the end
             if (!outputFile.fail()) {
                 outputFile << "</PRE>" << endl;
             }
         }
 
+        //close streams if they're open
         if (inputFile.is_open()) {
             inputFile.close();
         }
@@ -80,35 +100,51 @@ string HTMLConverter::writeToFile(string inputName, string outputName) {
             outputFile.close();
         }
     }
+        //catch any errors if they exist
     catch(ifstream::failure&) {
         return "Bad input file";
     }
 
+    //return file was written successfully
     return "\nFile written succesfully!";
 }
 
+//convert < to &lt; and > to &gt;
 string HTMLConverter::convertLine(string line) {
+    //assume there are some more chars to convert
     bool moreChars = true;
+    //while there are some
     while (moreChars) {
+        //get the position of <
         size_t charFound = line.find('<');
+        //if one was found
         if (charFound != string::npos) {
+            //replace < with &lt;
             line.replace(charFound, 1, "&lt;");
         }
+            //if none were found, move on
         else {
             moreChars = false;
         }
     }
 
+    //assume there are chars to convert
     moreChars = true;
+    //while there are some
     while (moreChars) {
+        //get the position of >
         size_t charFound = line.find('>');
+        //if one was found
         if (charFound != string::npos) {
+            //replace > with &gt;
             line.replace(charFound, 1, "&gt;");
         }
+            //if none were found, move on
         else {
             moreChars = false;
         }
     }
 
+    //return the line
     return line;
 }
--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -3,29 +3,42 @@
 
 #include <iostream>
 #include "HTMLConverter.h"
-#include <iostream>
 
 using namespace std;
 
 int main() {
+    //set move on to false
     bool moveOn = false;
+
+    //display welcome message
     cout << "Welcome to the HTML Converter Program!" << endl;
     cout << "This program will read in a .cpp file in output a .html file that can be displayed in a browser." << endl;
+
+    //ask user for input file name until its valid
     string inputFile;
     while (!moveOn) {
+        //prompt user
         cout << "\n\nPlease enter the name of the .cpp file you would like to read in: " << endl;
+        //tell user how file should be read
         cout << "NOTE: you must give the full file path of the file!!! (i.e. /home/Desktop/example.cpp)" << endl;
+        //read in file name
         getline(cin, inputFile);
 
+        //check if file name is valid and exists
         bool hasName = HTMLConverter::checkInputFileName(inputFile);
         bool fileExists = HTMLConverter::readFile(inputFile);
 
-        if (!inputFile.empty() && hasName && fileExists) {
+        //if valid and exists
+        if (hasName && fileExists) {
+            //move on
             moveOn = true;
+            //otherwise reprompt user
         } else {
             cout << "You must type a valid file name!" << endl;
         }
     }
+
+    //ask user for output file name and verify its a valid .html file
     moveOn = false;
     string outputFile;
     while (!moveOn) {
@@ -42,8 +55,10 @@ int main() {
         }
     }
 
+    //convert file to html file and write it to it
     HTMLConverter::writeToFile(inputFile, outputFile);
 
+    //display end message for user
     cout << "File written succesfully!" << endl;
     return 0;
 }
